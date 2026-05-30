d2a37fa3c833a257d6bb5c6d2028a6dda2501238
Sites <abdullahmahercacul@gmail.com>
2026-05-30 20:53:17 +0100

    Create firebase-messaging-sw.js

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,19 @@
+importScripts("https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js");
+
+firebase.initializeApp({
+  apiKey: "AIzaSyBl1uMOvD5zwvwnOoVBee5sQAx7J0nJyxA",
+  authDomain: "admin-biscuit.firebaseapp.com",
+  projectId: "admin-biscuit",
+  messagingSenderId: "429461746107",
+  appId: "1:429461746107:web:91512d7afd7b1b8d7b949e"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  self.registration.showNotification(payload.notification.title, {
+    body: payload.notification.body,
+    icon: "/icon.png"
+  });
+});
